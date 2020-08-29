cf7d7b1f923b3557d96824746f0f1cd69b4dfb80
Ankita-Guha <69446438+Ankita-Guha@users.noreply.github.com>
2020-08-29 20:53:02 +0530

    first website

---
+++ contact.php
@@ -0,0 +1,8 @@
+<?php
+
+  $username = filter_input(INPUT_POST,'USERNAME');
+  $email= filter_input(INPUT_POST,'email');
+  $phone= filter_input(INPUT_POST,'phone');
+  $message= filter_input(INPUT_POST,'message');
+
+?>
